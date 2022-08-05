333f327bcc050263bf9fbd5be37258121f3b040b
”Dong98-code“ <1094108253@qq.com>
2022-08-05 09:56:34 +0800

    输出题

--- notes/前端练习/js练习/输出是什么？/test.js
+++ notes/前端练习/js练习/输出是什么？/test.js
@@ -1,34 +1,9 @@
-// const async1 = async () => {
-//     console.log('async1');
-//     setTimeout(() => {
-//         console.log('timer1')
-//     }, 2000)
-//     await new Promise(resolve => {
-//         console.log('promise1')
-//     })
-//     console.log('async1 end')
-//     return 'async1 success'
-// }
-// console.log('script start');
-// async1().then(res => console.log(res));
-// console.log('script end');
-// Promise.resolve(1)
-//     .then(2)
-//     .then(Promise.resolve(3))
-//     .catch(4)
-//     .then(res => console.log(res))
-// setTimeout(() => {
-//     console.log('timer2')
-// }, 1000)
-// const async1 = async () => {
-//     console.log(new Promise(resolve => {
-//         console.log(1111);
-//     }))
-//     // console.log(res);
-//     // return "2222"
-//     // return res
-// }
-// // console.log(async1());
-// async1()
+let randomValue = { name: "Lydia" }
+randomValue = 23
 
-console.log(new Promise(resolve => { })); // 直接new promise为pending状态
+// if (!typeof randomValue === "string") {
+// 	console.log("It's not a string!")
+// } else {
+// 	console.log("Yay it's a string!")
+// }
+console.log(randomValue);
