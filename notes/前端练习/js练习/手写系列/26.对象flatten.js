a3edee0c26d2d667006ba2c6a2f2ba7fdc59df73
”Dong98-code“ <1094108253@qq.com>
2022-08-29 16:19:28 +0800

    feat:对象faltten

--- notes/前端练习/js练习/手写系列/26.对象flatten.js
+++ notes/前端练习/js练习/手写系列/26.对象flatten.js
@@ -27,25 +27,40 @@ function flatten(obj) {
     let res = {}
 
     // 遍历
-    function baseFlatten(item, preKey="") {
+    // function baseFlatten(item, preKey="") {
+    //     for (let [key, value] of Object.entries(item)) {
+    //         let newKey = key;
+    //         if (Array.isArray(item)) {
+    //             // 对应的值为数组
+    //             newKey = preKey ? `${preKey}[${key}]` : key;
+    //         } else {
+    //             newKey = preKey ? `${preKey}.${key}` : key
+    //         }
+
+    //         if (value && typeof value === 'object'){
+    //             baseFlatten(value , newKey)
+    //           }else{
+    //             res[newKey] = value
+    //           }
+        
+    //     }
+    // }
+
+    function baseFlatten(item, preKey = "") {
         for (let [key, value] of Object.entries(item)) {
             let newKey = key;
             if (Array.isArray(item)) {
-                // 对应的值为数组
                 newKey = preKey ? `${preKey}[${key}]` : key;
             } else {
-                newKey = preKey ? `${preKey}.${key}` : key
+                newKey = preKey ? `${preKey}.${key}` : key;
             }
-
-            if (value && typeof value === 'object'){
-                baseFlatten(value , newKey)
-              }else{
-                res[newKey] = value
-              }
-        
+            if (value && typeof value === "object") {
+                baseFlatten(value, newKey);
+            } else {
+                res[newKey] = value;
+            }
         }
     }
-
     baseFlatten(obj);
     return res;
     
